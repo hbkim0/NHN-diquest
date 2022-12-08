2cc53f9a55b923494aab7ec5630b5fea66071074
hbkim0 <hbkim0@diquest.com>
2022-12-08 10:08:20 +0900

    Python-study

--- Study.py
+++ Study.py
@@ -1,5 +1,28 @@
-# format
+## format
 print('{} {}'.format('one','two'))
 print('{} {}'.format(123, 450))
 print('{1} {0}'.format('one','two'))
 print('{:10.5}'.format('pythonstudy')) # 10자리의 자릿수 중에서 5개만 보여줌
+
+tuple = (1,2,3)
+set = {1,2,3} # or 1,2,3
+dict = {"123":123}
+
+## naming
+'''
+camel case : numberOfCollegeGraduates -> Method
+Pascal case : NumberOfCollegeGraduates -> Class
+Snake case : number_of_college_graduates -> 변수(파이썬에서 자주 사용)
+'''
+
+## 연산
+'''
+pow(2,3) = 2 ** 3 (2의 3제곱)
+a = [1,2,3]
+print(a.pop()) = 3 (a = [1,2] / 마지막 원소 제거 )
+a.count(3) = 1
+a.count("사과") = ??
+a.extend([4,5]) / a = [1,2,3,4,5] (원소 추가)
+'''
+
+
